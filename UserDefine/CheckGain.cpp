c830d0632b165be64cc75ed0b2b162acb9b73612
John <liangzheng1021@163.com>
2019-05-24 16:50:54 +0800

    Modify CheckGain

--- UserDefine/CheckGain.cpp
+++ UserDefine/CheckGain.cpp
@@ -1,38 +1,68 @@
 #include "FEBDAQMULT.C"
 #include <string>
+#include <iostream>
 using namespace std;
 
-void CheckGain(const char * iface = "enp5s0f0")
-{
-    FEBDAQMULT(iface);
-    UpdateConfig();
-    HVON();
+int fInit = 0;
 
-    for(int bias:{0, 30, 60, 90, 120, 150, 180, 210, 240})      // Set different bias
+void CheckGain2(int group);
+void CheckGain2Loop(int startIndex)
+{
+    if(!fInit)
     {
-        fChanEnaTrig[32] -> SetOn();
+        FEBDAQMULT("enp5s0f0");
+        UpdateConfig();
+        fInit = 1;
+    }
+
+    for(int group = startIndex; group < 16; group++)
+    {
+        CheckGain2(group);
+    }
+}
+
+void CheckGain2(int group)
+{
+    if(!fInit)
+    {
+        FEBDAQMULT("enp5s0f0");
+        UpdateConfig();
+        fInit = 1;
+    }
+
+        string sTemp;
+        HVOF();
+        gSystem -> ProcessEvents();
+        cout << "Measuring group: " << group << " Please check your connection, and press enter to continue" << endl;
+        cin >> sTemp;
+        HVON();
+        fChanEnaTrig[32] -> SetOn(kFALSE);
         // fChanEnaTrig[32] -> SetOn(kFALSE);
         Reset();
         SendConfig();
         gSystem -> ProcessEvents();
         gSystem->Sleep(1000);
-        for (int i = 0; i < 32; i++)    // Set bias and set amplifier
-        {
-            fChanBias[i]->SetNumber(bias);
-            fChanGain[i]->SetNumber(64);
-            fChanEnaTrig[i] -> SetOn(kFALSE);
-        }
-        SendConfig();
-        gSystem -> ProcessEvents();
-        gSystem->Sleep(1000);
 
-        for(int group = 0; group < 16; group++)
+        for(int bias:{0, 30, 60, 90, 120, 150, 180, 210, 240})      // Set different bias
         {
             int ch1 = group * 2;
-            int ch2 = group * 2 + 1;
+            int ch2 = ch1 + 1;
 
+            for (int i = 0; i < 32; i++)    // Set bias and set amplifier
+            {
+                fChanBias[i]->SetNumber(0);
+                fChanGain[i]->SetNumber(50);
+                fChanEnaTrig[i] -> SetOn(kFALSE);
+            }
+            fChanBias[ch1] -> SetNumber(bias);
+            fChanBias[ch2] -> SetNumber(bias);
+
+            SendConfig();
+            gSystem -> ProcessEvents();
+            gSystem->Sleep(1000);
             fChanEnaTrig[ch1] -> SetOn();
             fChanEnaTrig[ch2] -> SetOn();
+
             if(group != 0)
             {
                 fChanEnaTrig[ch1 - 2] -> SetOn(kFALSE);
@@ -48,10 +78,10 @@ void CheckGain(const char * iface = "enp5s0f0")
             string filename1 = (string) "Bias-" + to_string(bias) + "Ch-" + to_string(ch1);
             string filename2 = (string) "Bias-" + to_string(bias) + "Ch-" + to_string(ch2);
 
-            hst[ch1]->SaveAs((filename1 + ".pdf").c_str());
+            c -> cd(ch1) -> SaveAs((filename1 + ".pdf").c_str());
+            c -> cd(ch2) -> SaveAs((filename2 + ".pdf").c_str());
             hst[ch1]->SaveAs((filename1 + ".root").c_str());
-            hst[ch2]->SaveAs((filename2 + ".pdf").c_str());
             hst[ch2]->SaveAs((filename2 + ".root").c_str());
         }
-    }
+
 }
