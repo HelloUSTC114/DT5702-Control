0d84f2c6046f6b825cc36aa1b2dd8317d70648d4
John <liangzheng1021@163.com>
2019-05-30 01:25:14 +0800

    Add temperary processing histo

---
+++ UserDefine/CheckGain-32SiPM/DrawAllCh.cpp
@@ -0,0 +1,24 @@
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TH1.h"
+void DrawAllCh()
+{
+    auto c = new TCanvas("c", "c", 3000, 2500);
+    c -> Divide(4, 8);
+    TH1F *hArray[32]{0};
+    for(int i = 2; i < 32; i++)
+    {
+        auto file = new TFile(Form("Bias-0Ch-%d.root", i));
+
+        auto h = (TH1F*)file -> Get(Form("h%d", i));
+        h -> SetDirectory(NULL);
+        hArray[i] = h;
+        delete file;
+    }
+    for(int i = 2; i < 32; i++)
+    {
+        c -> cd(i);
+        hArray[i] -> Draw();
+    }
+    c -> SaveAs("Test.pdf");
+}
